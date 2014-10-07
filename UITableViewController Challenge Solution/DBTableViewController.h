b90740818f426562526f849400d5cff4b8468b9f
aymanmt <aymanmt@gmail.com>
2014-10-07 13:39:47 +0300

    added new files as well as modified others

--- UITableViewController Challenge Solution/DBTableViewController.h
+++ UITableViewController Challenge Solution/DBTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  DBTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by admin on 10/7/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface DBTableViewController : UITableViewController
 
 @end
-
